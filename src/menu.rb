48657a43fcddfdbe27a104316c544c7bd241af8f
Adriana Heesh <adrianaheesh@gmail.com>
2020-09-28 11:20:28 +1000

    added a help menu

--- src/menu.rb
+++ src/menu.rb
@@ -1,2 +1,12 @@
 require 'thor'
 
+class Menu < Thor 
+    class_option :help, type: :string, aliases: "-h"
+    
+    desc "help", "displays how to use the app"
+    def help
+        puts "This is a help message"
+    end 
+end
+
+Menu.start(ARGV)
